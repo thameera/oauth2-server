c3c9618853d132391f625dbd324107e43fb31bfa
Thameera Senanayaka <thameera123@gmail.com>
2018-10-06 21:39:53 +0530

    Client ID validation

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const CLIENTS = [
+  {id: '1', name: 'Default client'},
+  {id: '2', name: 'Client 2'}
+]
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -10,6 +15,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/authorize', (req, res) => {
+  /* ClientID validations */
+  const clientId = req.query.client_id
+  if (!clientId) {
+    return res.render('error', {message: 'Client ID missing'})
+  }
+  const client = CLIENTS.find(c => c.id === clientId)
+  if (!client) {
+    return res.render('error', {message: `Invalid client ID: ${clientId}`})
+  }
+  console.log(client)
+
   res.render('error', {message: '/authorize not fully implemented'})
 })
 
