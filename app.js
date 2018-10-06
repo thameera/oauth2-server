48d2fb74e28049a5be14118cdd6cd9f34987dacb
Thameera Senanayaka <thameera123@gmail.com>
2018-10-06 21:34:26 +0530

    Add ejs and basic error page

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.send('OIDC server')
+})
+
+app.get('/authorize', (req, res) => {
+  res.render('error', {message: '/authorize not fully implemented'})
 })
 
 const port = process.env.PORT || 8400
